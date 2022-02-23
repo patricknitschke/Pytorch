38354b1413d71bfd43f4f8373337cf452f3173f7
Patrick Nitschke <patrick.nitschke8@gmail.com>
2022-02-23 17:09:52 +0100

    vae on depth images implemented

--- VAE/di_dataset.py
+++ VAE/di_dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 
 import os
 import numpy as np
@@ -12,30 +13,37 @@ batch_size = 4
 # load_paths = [os.path.join(saves_folders, saves_folder) for saves_folder in os.listdir(saves_folders)]
 
 class DepthImageDataset(Dataset):
-    def __init__(self, load_paths) -> None:
+    def __init__(self, load_path) -> None:
         super(DepthImageDataset, self).__init__()
         
-        self.x = []
+        self.x = torch.empty(0)
         self.nb_images = 0
-        for load_path in load_paths: 
-            nb_files = int(len([f for f in os.listdir(load_path) if f.endswith('.p') and os.path.isfile(os.path.join(load_path, f))]) / 5) # five dicts
-            print(f"Loading from {load_path}")
-            for i in tqdm(range(nb_files)):
-                di_load           = pickle.load(open(load_path + "/di_dump" + str(i) + ".p", "rb"))
-                # obs_load          = pickle.load(open(load_path + "/obs_dump" +str(i) + ".p", "rb"))
-                # action_load       = pickle.load(open(load_path + "/action_dump" + str(i) + ".p", "rb"))
-                # action_index_load = pickle.load(open(load_path + "/action_index_dump" + str(i) + ".p", "rb"))
-                # collision_load    = pickle.load(open(load_path + "/collision_dump" + str(i) + ".p", "rb"))
-         
+        nb_files = int(len([f for f in os.listdir(load_path) if f.endswith('.p') and os.path.isfile(os.path.join(load_path, f))]) / 5) # five dicts
+        print(f"Loading from {load_path}")
+        for i in tqdm(range(1)):
+#                 obs_load          = pickle.load(open(load_path + "/obs_dump" +str(i) + ".p", "rb"))
+#                 action_load       = pickle.load(open(load_path + "/action_dump" + str(i) + ".p", "rb"))
+#                 action_index_load = pickle.load(open(load_path + "/action_index_dump" + str(i) + ".p", "rb"))
+#                 collision_load    = pickle.load(open(load_path + "/collision_dump" + str(i) + ".p", "rb"))
+            di_load           = pickle.load(open(load_path + "/di_dump" + str(i) + ".p", "rb"))
+            
+            x_ = []
             for ep in di_load.values():
                 for image in ep:
-                    image_flipped = np.flip(image, 1)
-                    self.x.append(image.transpose(2,0,1))
-                    self.x.append(image_flipped.transpose(2,0,1))
-                    
+                    image = image / 256
+                    image = image.transpose(2, 0, 1)
+                    image = np.pad(image, [(0,0), (1, 1), (0, 0)], mode='constant', constant_values=0)
+                    image_flipped = np.flip(image, 2)
+                    x_.append(image)
+                    x_.append(image_flipped)
+
                     self.nb_images += 2
+                    if self.nb_images > 1:
+                        break
+                if self.nb_images > 1:
+                    break
         
-        self.x = torch.Tensor(np.array(self.x))
+            self.x = torch.cat((self.x, torch.Tensor(np.array(x_))))
     
     def __getitem__(self, index: int):
         return self.x[index]
